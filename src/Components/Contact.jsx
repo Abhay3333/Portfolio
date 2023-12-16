4fa2b866a047fe4f6283ea343a918149b991b5e3
Abhay Hemant Upadhaya <abhayupadhaya829@gmail.com>
2023-12-17 02:16:09 +0530

    update

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -28,13 +28,16 @@ const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("https://portfolio-backend-app.onrender.com/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
+    let response = await fetch(
+      "https://portfolio-backend-app.onrender.com/contact",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(formDetails),
+      }
+    );
     setButtonText("Send");
     let result = await response.json();
     setFormDetails(formInitialDetails);
